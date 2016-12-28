a2e8c73ffef725dcb90cebe1d080981f186fb6b9
Sergey Ukustov <sergey@ukstv.me>
2016-12-28 17:37:29 +0300

    Basic keys

--- test/secret.js
+++ test/secret.js
@@ -1,11 +1,48 @@
-const assert = require("assert"),
-      secret = require("../lib/secret");
+"use strict";
 
-describe('secret', function() {
-    describe('.digest', function() {
-        it('should return -1 when the value is not present', function() {
-            let output = Buffer("2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae", "hex");
+const assert = require("assert"),
+      secret = require("../lib/secret"),
+      crypto = require("crypto");
+
+describe("secret", () => {
+    describe(".digest", () => {
+        it("compute SHA256 hash", () => {
+            let output = Buffer.from("2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae", "hex");
             assert.deepEqual(output, secret.digest("foo"));
+        });
+    });
+
+    describe(".PrivateKey", () => {
+        it("contains buffer", () => {
+            let randomBytes = crypto.randomBytes(32);
+            let key = new secret.PrivateKey(randomBytes);
+            assert.deepEqual(randomBytes, key.buffer());
+        })
+    });
+
+    describe(".PublicKey", () => {
+        it("contains buffer", () => {
+            let randomBytes = crypto.randomBytes(32);
+            let key = new secret.PublicKey(randomBytes);
+            assert.deepEqual(randomBytes, key.buffer());
+        })
+    });
+
+    describe(".Key", () => {
+        let randomPrivateKey = new secret.PrivateKey(crypto.randomBytes(32));
+        let randomPublicKey = secret.derivePublicKey(randomPrivateKey);
+        let key = new secret.Key(randomPrivateKey, randomPublicKey);
+
+        describe("#private()", () => {
+            it("return private key", () => {
+               assert.deepEqual(randomPrivateKey, key.private());
+            });
+        });
+
+        describe("#public()", () => {
+            it("return public key", () => {
+                assert.deepEqual(randomPublicKey, key.public());
+            });
         });
     });
 });
