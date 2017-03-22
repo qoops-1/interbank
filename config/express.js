f7c3365ddee18e92a2c6d70014a5de3435c7450c
n0cte <ilnur.a.g@gmail.com>
2017-03-22 18:52:02 +0300

    server send event middleware

--- config/express.js
+++ config/express.js
@@ -1,32 +1,32 @@
 "use strict";
 
-const cors   = require("cors");
+const cors = require("cors");
 const unless = require("express-unless");
-const jwt    = require("express-jwt");
-const redis  = require('./redis');
-const env    = require('./env');
+const jwt = require("express-jwt");
+const redis = require('./redis');
+const env = require('./env');
 
-function getToken(req){
+function getToken(req) {
   return req.headers['x-access-token'] || null;
 }
 
-function jwtMiddleware(){
-  let func = function(req, res, next){
+function jwtMiddleware() {
+  let func = function (req, res, next) {
     let token = getToken(req);
-    let ip    = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    if(!!!token){
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    if (!!!token) {
       return next(new Error("token not found"));
     }
     let rId = `${env.session.prefix}:${token}`;
-    redis.get(rId, function(err, reply){
-      if(err){
+    redis.get(rId, function (err, reply) {
+      if (err) {
         return next(new Error(err));
       }
-      if(!!!reply){
+      if (!!!reply) {
         return next(new Error("token doesn't exists"))
       }
       var data = JSON.parse(reply);
-      if(data.token!==token || data.ip!==ip){
+      if (data.token !== token || data.ip !== ip) {
         return next(new Error("token doesn't exists"))
       }
       req.user = data;
@@ -37,21 +37,38 @@ function jwtMiddleware(){
   return func;
 };
 
+function sseMiddleware() {
+  return function (req, res, next) {
+    res.sseSetup = function () {
+      res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+      });
+    };
+    res.sseSend = function (data) {
+      res.write(`data: ${JSON.stringify(data)}\n\n`);
+    };
+    next();
+  }
+}
+
 module.exports = (app) => {
-    if (process.env.CORS_ORIGIN) {
-        let corsOptions = {
-            origin: process.env.CORS_ORIGIN,
-            methods: process.env.CORS_METHODS || 'GET,PUT,POST,DELETE',
-            allowedHeaders: ['Content-Type', 'x-access-token']
-        };
-        app.use(cors(corsOptions));
-    }
-    app.disable('x-powered-by');
-    let jwtCheck = jwt({
-      secret: env.session.secret,
-      getToken: getToken
-    });
-    jwtCheck.unless = unless;
-    app.use(jwtCheck.unless({path: '/signin' }));
-    app.use(jwtMiddleware().unless({path: '/signin' }));
+  if (process.env.CORS_ORIGIN) {
+    let corsOptions = {
+      origin: process.env.CORS_ORIGIN,
+      methods: process.env.CORS_METHODS || 'GET,PUT,POST,DELETE',
+      allowedHeaders: ['Content-Type', 'x-access-token']
+    };
+    app.use(cors(corsOptions));
+  }
+  app.disable('x-powered-by');
+  let jwtCheck = jwt({
+    secret: env.session.secret,
+    getToken: getToken
+  });
+  jwtCheck.unless = unless;
+  app.use(jwtCheck.unless({ path: '/signin' }));
+  app.use(jwtMiddleware().unless({ path: '/signin' }));
+  app.use(sseMiddleware());
 };
