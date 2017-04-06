26031ed7f596a7e54e22e6a8cd223d3b2b257ae6
qoops <ilya.v.gladyshev@gmail.com>
2017-04-06 12:35:39 +0300

    one-way transfer contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,49 +1,32 @@
 pragma solidity ^0.4.8;
 
-import "./ERC20Token.sol";
+contract Ownable {
+  address public owner;
 
-contract Token is ERC20Token {
+  function Ownable() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    if (msg.sender != owner) {
+      throw;
+    }
+    _;
+  }
+
+}
+contract Token is Ownable {
     mapping(address => uint) balances;
     mapping (address => mapping (address => uint)) allowed;
-    uint256 supply;
 
-    function Token(uint _supply) {
-        totalSupply = _supply;
-    }
+    event Transfer(address indexed from, address indexed to, uint value);
 
-    function transfer(address _to, uint _value) returns (bool success) {
-        if (balances[msg.sender] < _value) {
-            return false;
-        }
-        balances[msg.sender] = balances[msg.sender] - _value;
+    function transfer(address _to, uint _value) onlyOwner {
         balances[_to] = balances[_to] + _value;
         Transfer(msg.sender, _to, _value);
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint _value) returns (bool success) {
-        var _allowance = allowed[_from][msg.sender];
-        if (_value > _allowance || _value > balances[_from]) {
-            return false;
-        }
-        balances[_to] = balances[_to] + _value;
-        balances[_from] = balances[_from] - _value;
-        allowed[_from][msg.sender] = _allowance - _value;
-        Transfer(_from, _to, _value);
-        return true;
     }
 
     function balanceOf(address _owner) constant returns (uint balance) {
         return balances[_owner];
-    }
-
-    function approve(address _spender, uint _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint remaining) {
-        return allowed[_owner][_spender];
     }
 }
